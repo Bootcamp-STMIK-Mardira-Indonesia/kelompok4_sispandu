6c457eee78a744f45df1074ee92eaf3655da013f
HarisHandiyana1 <harishandiyana2@gmail.com>
2023-01-18 13:07:14 +0700

    add new landing page and home_page with bootstrap and then will convert with angular js

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <title>Halaman | Landing Page</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <nav class="navbar navbar-expand-sm navbar-light bg-transparent">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/Pengadu/layout image/Texture.png" class="img-fluid rounded-top" alt="" style="width: 400px; margin-left:-1rem; margin-top:-1.5rem;">
+                    <img src="assets/Pengadu/image/Logo1.png" class="img-fluid rounded-top" alt="" style="margin-left:-350px;">
+                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+
+                        <div class="d-flex my-2 my-lg-0" style="margin-left: 1000px; ">
+                            <button type="button" class="btn btn-primary" style="display:flex; padding:10px; width:128px; height:40px; background:white;border-radius: 32px; padding:10px; color:blue; justify-content:center; align-items:center;margin-top:-70px;" name="register">Daftar</button>
+                            <button type="button" class="btn btn-primary" style="display:flex; padding:10px; width:128px; height:40px; background:linear-gradient(135deg, #000044 0%, #004E92 100%);border-radius: 32px; padding:10px; color:white; justify-content:center; align-items:center; margin-left:20px;margin-top:-70px;" name="login">Masuk</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </div>
+
+    <div class="content" style="margin-left:60px; margin-top:50px;">
+        <img src="assets/img/Content.png" class="img-fluid rounded-top" alt="" style="width: 500px;">
+        <img src="assets/img/Image.png" class="img-fluid rounded-top" alt="" style="width: 500px; margin-left:220px;">
+    </div>
+
+
+    <div class="slogan" style="margin-left: 60px; margin-top:-30px;">
+        <img src="assets/Pengadu/layout image/First On Top.png" class="img-fluid rounded-top" alt="" style="width: 140px;">
+        <img src="assets/Pengadu/layout image/5JT+ Lainnya telah melapor.png" class="img-fluid rounded-top" alt="" style="width: 140px;">
+    </div>
+    <div class="misi" style="margin-left: 60px;">
+        <img src="assets/Pengadu/layout image/misi.png" class="img-fluid rounded-top" alt="" style="width: 1210px; margin-top:40px;">
+    </div>
+
+    <div class="image1">
+        <img src="assets/Pengadu/Image/Logo3.png" class="img-fluid rounded-top" alt="" style="width:180px; margin-left:250px; margin-top:50px;">
+        <div class="alamat" style="margin-left:600px;">
+            <div style="margin-top: -140px;">
+                <img src="assets/Pengadu/icon/map.png" alt="" style="margin-top:20px;">
+                <img src="assets/Pengadu/icon/Jl. Soekarno-Hatta No. 211 Leuwipanjang, Situsaeur Kec. Bojongloa Kidul, Kota Bandung Jawa Barat 40233, Indonesia.png" class="img-fluid rounded-top" alt=""><br>
+            </div>
+            <div style="margin-top: 22px;">
+                <img src="assets/Pengadu/icon/call.png" class="img-fluid rounded-top" alt="">
+                <img src="assets/Pengadu/icon/+(62) 22-5230382.png" class="img-fluid rounded-top" alt=""><br>
+            </div>
+            <div style="margin-top:22px;">
+                <img src="assets/Pengadu/icon/sms.png" class="img-fluid rounded-top" alt="">
+                <img src="assets/Pengadu/icon/Homepage@stmik-mi.ac.id.png" class="img-fluid rounded-top" alt="">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+</body>
+<script src="assets/js/bootstrap.js"></script>
+
+</html>
